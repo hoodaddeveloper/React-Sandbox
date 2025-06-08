cb5851220fc8318cf27925034f1c5227ee0636d8
hoodaddeveloper <mohammadkhaffas@gmail.com>
2025-06-08 02:27:23 +0200

    copied some old files

---
+++ old-react-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.Fragment>
+    <header>header</header>
+    <section>content</section>
+    <footer>footer</footer>
+  </React.Fragment>
+)
+
+
