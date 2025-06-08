cbe36255834f46ceb141b927caf3b9ef0b93afdc
hoodaddeveloper <mohammadkhaffas@gmail.com>
2025-06-08 02:43:28 +0200

    practicing react

--- old-react-app/src/index.js
+++ old-react-app/src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.Fragment>
+  <React.StrictMode>
     <header>header</header>
     <section>content</section>
     <footer>footer</footer>
-  </React.Fragment>
-)
-
-
+  </React.StrictMode>
+);
