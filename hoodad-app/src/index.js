3ff3317a1f5bf52214dce7c872bf38e7422b2e27
hoodaddeveloper <mohammadkhaffas@gmail.com>
2025-06-16 02:13:57 +0200

    jsx = js & xml

--- hoodad-app/src/index.js
+++ hoodad-app/src/index.js
@@ -6,5 +6,6 @@ root.render(
   <React.StrictMode>
     <h1>Hello, Hoodad!</h1>
     <p>Welcome to the Hoodad app.</p>
+    <button onClick={() => alert('Button clicked!')}>Click Me!</button>
   </React.StrictMode>
 );
